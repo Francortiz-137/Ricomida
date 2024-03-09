9c4f58c073ee41a2cb33c2c1161f8112953ec257
Francortiz-137 <francortiz.c@gmail.com>
2024-03-08 21:24:17 -0300

    added card toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     
     $('[data-toggle="tooltip"]').tooltip();
+    
     $('#enviarCorreo').click(()=>{
         alert('El correo fue enviado correctamente...');
     })
     
+    $('h4').on('dblclick', function() {
+        $(this).css('color', 'red');
+    });
+
+    $('.card-title').on('click',function(){
+        console.log('xd');
+        $(this).next('.card-text').toggle();
+    });
 });
