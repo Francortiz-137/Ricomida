3d44d959b73d23d22aae9de9d2fc560090686998
Francortiz-137 <francortiz.c@gmail.com>
2024-03-08 21:06:35 -0300

    tooltips + alert

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    
+    $('[data-toggle="tooltip"]').tooltip();
+    $('#enviarCorreo').click(()=>{
+        alert('El correo fue enviado correctamente...');
+    })
+    
+});
